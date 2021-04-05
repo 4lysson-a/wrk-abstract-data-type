759a752f4e214d7c9f484f3e74039c674e65ef3c
4ly-a <4ly.alcantara@gmail.com>
2021-04-05 11:32:20 -0300

    ex2 main

---
+++ ex2_vetores/main.c
@@ -0,0 +1,21 @@
+// Desenvolva e implemente um TAD que represente um 
+// conjunto de inteiros. Para isso, utilize um vetor 
+// de inteiros. O TAD deverá conter as seguintes funções
+
+// Criar um conjunto vazio.
+// Remover um elemento no conjunto.
+// Inserir um elemento no conjunto.
+// União de dois conjuntos.
+// Menor valor do conjunto.
+// Testar se o conjunto é vazio.
+// Testar se os dois conjuntos são iguais.
+// Testar se um número pertence ao conjunto.
+
+// Para cada função, explique brevemente a ideia
+
+#include "./lib/tad.h"
+
+int main()
+{
+  return 0;
+}
